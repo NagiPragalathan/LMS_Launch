ab5f74b034abcb30179a5336a7c124f292d867ec
nagipragalathan <kk2069037@gmail.com>
2023-09-11 08:38:01 +0530

    Button information are updated

--- base/models.py
+++ base/models.py
@@ -3,10 +3,5 @@ from django.utils import timezone
 
 # Create your models here.
 
-class Pages(models.Model):
+class Toggle(models.Model):
     name = models.CharField(max_length=50)
-    description = models.CharField(max_length=100)
-    html = models.TextField()
-    css = models.TextField()
-    image = models.CharField(max_length=50)
-    preview_link = models.TextField()
--- base/views.py
+++ base/views.py
@@ -2,17 +2,18 @@
 import json
 from django.shortcuts import render
 from django.http import JsonResponse
-
+from .models import Toggle
 def update_text(request):
     if request.method == 'POST':
         try:
             data = json.loads(request.body.decode('utf-8'))
             new_text = data.get('newText')
             if new_text:
+                Toggle.name
                 request.session['text'] = new_text
                 return JsonResponse({'status': 'success'})
         except json.JSONDecodeError:
-            pass
+            print("Error..1")
     
     return JsonResponse({'status': 'error'})
 
@@ -20,7 +21,7 @@ def get_text(request):
     try:
         return JsonResponse({'text': request.session['text']})
     except:
-        return JsonResponse({'text': 'close'})
+        return JsonResponse({'text': 'close1'})
         
 
 def interface_with_button(request):
